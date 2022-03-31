13b08c4036929aed135030a0263b2748b0c41d35
matth <matth@192.168.1.10>
2022-03-31 22:02:23 +1300

    Ok this has finally fixed it

---
+++ src/Monster.java
@@ -0,0 +1,12 @@
+
+public class Monster {
+	public void sayHello() {
+		System.out.println("Hi");
+	}
+	
+	public static void main(String[] args) {
+		Monster test = new Monster();
+		test.sayHello();
+	}
+
+}
