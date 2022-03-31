bd29b93ec9d59c918e8ecf2a8d995308c9f3c29f
matth <matth@192.168.1.10>
2022-03-31 21:11:49 +1300

    More testing

---
+++ MonsterFighter-Seng201/mainClasses/Monster.java
@@ -0,0 +1,10 @@
+
+public class Monster {
+	public void hello() {
+		System.out.println("hi");
+	}
+	public static void main(String[] args) {
+		Monster test = new Monster();
+		test.hello();
+	}
+}
