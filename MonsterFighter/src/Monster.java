f71b11e856abceeb4eeaf62335af32e7cfbf4801
Angelo  Gasmin <Angelo  Gasmin@10.196.29.57>
2022-04-05 13:19:10 +1200

    d

--- MonsterFighter/src/Monster.java
+++ MonsterFighter/src/Monster.java
@@ -2,11 +2,13 @@
 public class Monster {
 	public void sayHello() {
 		System.out.println("Hi");
+		System.out.println("sfkljs");
 	}
 	
 	public static void main(String[] args) {
 		Monster test = new Monster();
 		test.sayHello();
+		
 	}
 
 }
