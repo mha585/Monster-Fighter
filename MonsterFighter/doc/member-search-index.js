39d2800d91c21cff22dd316bf55341ba2001e958
mha585 <mha585@uclive.ac.nz>
2022-04-05 14:42:00 +1200

    setup html stuff

---
+++ MonsterFighter/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Battles","l":"Battles()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Battles","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"Battles","l":"sayHello()"}];updateSearchResults();
--- MonsterFighter/src/Battles.java
+++ MonsterFighter/src/Battles.java
@@ -1,5 +1,15 @@
-
+/**
+ * This class implements a Battle
+ *
+ * @author Matthew Harper
+ * @version 1.1, Feb 2022.
+ */
 public class Battles {
+	/**
+	 * Says Hello.
+	 */
+	
+	
 	public void sayHello() {
 		System.out.println("Hi");
 		System.out.println("Battles happen here");
