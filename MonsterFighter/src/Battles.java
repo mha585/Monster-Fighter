f420d6dbb1ba5acbb83350b994dc708d0e239595
matth <matth@10.196.58.219>
2022-04-05 13:45:05 +1200

    Test

--- MonsterFighter/src/Battles.java
+++ MonsterFighter/src/Battles.java
@@ -1,4 +1,13 @@
 
 public class Battles {
-
+	public void sayHello() {
+		System.out.println("Hi");
+		System.out.println("Battles happen here");
+	}
+	
+	public static void main(String[] args) {
+		Battles test = new Battles();
+		test.sayHello();
+		
+	}
 }
