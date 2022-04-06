6c78b9d8f17dccff088dd90256053323135ac368
mha585 <mha585@uclive.ac.nz>
2022-04-06 15:28:52 +1200

    added more comments

--- MonsterFighter/src/Purchasable.java
+++ MonsterFighter/src/Purchasable.java
@@ -1,13 +1,21 @@
 /**
  * This interface describes purchasable classes
  * 
- * @author Angelo  Gasmin
+ * @author Angelo Gasmin
  * @version 1.0
  */
 public interface Purchasable {
-	
+	/**
+	 * Gets the description of the current item.
+	 */
 	void getDescription(String descr);
+	/**
+	 * Gets the price to purchase the current item.
+	 */
 	void getPrice(int cost);
+	/**
+	 * Gets the price to sell the current item.
+	 */
 	void sellPrice(int sell);
 
 }
