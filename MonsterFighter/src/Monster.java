1e94b4b902fc800197858b8e801f2b59c39247ba
mha585 <mha585@uclive.ac.nz>
2022-04-06 16:47:53 +1200

    Created version 1 of Monster and PartyMonster

--- MonsterFighter/src/Monster.java
+++ MonsterFighter/src/Monster.java
@@ -1,14 +1,83 @@
-
+/**
+ * This class describes the Monster superclass
+ * 
+ * @author Matthew Harper
+ * @version 1.0
+ */
 public class Monster {
-	public void sayHello() {
-		System.out.println("Hi");
-		System.out.println("sfkljs");
+	/**
+	 * The name of the Monster
+	 */
+	private String myName;
+	/**
+	 * The max health of the Monster
+	 */
+	private double myMaxHealth;
+	/**
+	 * The current health of the Monster
+	 */
+	private double myCurrentHealth;
+	/**
+	 * The heal amount of the Monster
+	 */
+	private double myHealAmount;
+	/**
+	 * The damage the Monster deals
+	 */
+	private double myDamage;
+	/**
+	 * The current level / teir of the Monster
+	 */
+	private int myTeir;
+	/**
+	 * The basic constructor for the Monster class
+	 * @param name			The name of the Monster
+	 * @param maxHealth		The maxHealth of the Monster
+	 * @param healAmount	The healAmount of the Monster
+	 * @param damage		The amount of damage the Monster does
+	 * @param teir			The level of the Monster
+	 */
+	public Monster(String name, double maxHealth, double healAmount, double damage, int teir) {
+		myName = name;
+		myMaxHealth = maxHealth;
+		myCurrentHealth = myMaxHealth;
+		myHealAmount = healAmount;
+		myDamage = damage;
+		myTeir = teir;
+	}
+	
+	public int getTeir() {
+		return myTeir;
+	}
+	
+	public void setTeir(int teirGained) {
+		myTeir += teirGained;
+	}
+	
+	/**
+	 * Sets the health of the Monster
+	 * if statement to ensure that the current health does not exceed the max health
+	 * @param healthChange	The amount the health of the monster changes by
+	 */
+	public void setHealth(double healthChange) {
+		if ((myCurrentHealth + healthChange) <= myMaxHealth) {
+			myCurrentHealth += healthChange;
+		} else {
+			myCurrentHealth = myMaxHealth;
+		}
+//		probably do an if statement here to see if the monseter isDead?
+	}
+	
+	public String toString() {
+		return "my name is " + myName + " and my current health is " + myCurrentHealth +
+				" currently I am teir " + myTeir;
 	}
 	
 	public static void main(String[] args) {
-		Monster test = new Monster();
-		test.sayHello();
-		
+		Monster test = new Monster("Test", 100, 5, 5, 2);
+		System.out.println(test);
+		test.setHealth(-42);
+		System.out.println(test);
 	}
 
 }
--- MonsterFighter/src/PartyMonsters.java
+++ MonsterFighter/src/PartyMonsters.java
@@ -1,4 +1,57 @@
-
-public class PartyMonsters {
-
+/**
+ * This class describes the PartyMonsters class
+ * this is all the monsters in the players party
+ * these PartyMonsters are the same as Monsters
+ * however they have the ability to level up and get stronger
+ * 
+ * @author Matthew Harper
+ * @version 1.0
+ */
+public class PartyMonsters extends Monster {
+	/**
+	 * The amount of experience of the PartyMonster
+	 */
+	private double myExperience;
+	
+	/**
+	 * The basic constructor for the PartyMonsters class
+	 * also initialises myExperience to be 0
+	 * @param name			The name of the Monster
+	 * @param maxHealth		The maxHealth of the Monster
+	 * @param healAmount	The healAmount of the Monster
+	 * @param damage		The amount of damage the Monster does
+	 * @param teir			The level of the Monster
+	 */
+	public PartyMonsters(String name, double maxHealth, double healAmount, double damage, int teir) {
+		super(name, maxHealth, healAmount, damage, teir);
+		myExperience = 0;
+	}
+	
+	public double getMyExperience() {
+		return myExperience;
+	}
+	
+	/**
+	 * Sets the experience of the PartyMonster
+	 * checks if the experience is above 100 and if it is the monster levels up
+	 * @param healthChange	The amount the health of the monster changes by
+	 */
+	public void gainExperience(double experience) {
+		myExperience += experience;
+		if (myExperience >= 100) {
+			super.setTeir((int) (myExperience / 100));
+			myExperience = (myExperience % 100);
+		}
+	}
+	
+	public String toString() {
+		return "my teir is " + super.getTeir() + " my experience is " + getMyExperience();
+	}
+	
+	public static void main(String[] args) {
+		PartyMonsters test = new PartyMonsters("Test", 100, 5, 5, 2);
+		System.out.println(test);
+		test.gainExperience(1020);
+		System.out.println(test);
+	}
 }
