fbdab849aadb6c3c90961e212e9cb9824df260bb
mha585 <mha585@uclive.ac.nz>
2022-04-12 17:13:27 +1200

    Added two new types of Monsters

---
+++ MonsterFighter/src/GlassMonster.java
@@ -0,0 +1,55 @@
+
+public class GlassMonster extends Monster{
+	/**
+	 * The description of GlassMonster
+	 */
+	private static String description = "A glass cannon, effective against all but easily slain";
+	/**
+	 * The price of GlassMonster
+	 */
+	private static int price = 250;
+	/**
+	 * The selling price of GlassMonster
+	 */
+	private static int sell = 200;
+	/**
+	 * generates a random number in a given range
+	 */
+	private randomGen number = new randomGen();
+	/**
+	 * The basic constructor for the GlassMonster class
+	 * used when all values but type are given
+	 * manually updates the type to "Glass"
+	 * @param name			The name of the GlassMonster
+	 * @param maxHealth		The maxHealth of the GlassMonster
+	 * @param healAmount	The healAmount of the GlassMonster
+	 * @param damage		The amount of damage the GlassMonster does
+	 * @param speed			The speed of the GlassMonster
+	 * @param tier			The level of the GlassMonster
+	 * @param sell			Selling price of the GlassMonster
+	 * @param cost			Price of the GlassMonster
+	 * @param description	Description of the GlassMonster
+	 */
+	public GlassMonster(String name, double maxHealth, double healAmount, double damage, double speed, int Tier, 
+			int sell, int cost, String description) {
+		super(name, maxHealth, healAmount, damage, speed, Tier, sell, cost, description);
+		super.setType("Glass");
+	}
+	/**
+	 * The empty constructor for the GlassMonster class
+	 * this gets called when no params are entered.
+	 */
+	public GlassMonster() {
+		super(price, sell, description);
+		super.setType("Glass");
+		super.gainMaxHealth((-1 * (super.getMaxHealth())) + number.randNumInRange(0, (int) super.getMaxHealth() / 2));
+		super.gainHealth((-1 * (super.getHealth())) + super.getMaxHealth());
+		super.gainDamage(super.getDamage() + 10);
+	}
+	/**
+	 * Generates a string representation of the GlassMonster
+	 */
+	public String toString() {
+		return super.toString() + "\nType: " + super.getType();
+	}
+}
--- MonsterFighter/src/testFight.java
+++ MonsterFighter/src/testFight.java
@@ -7,13 +7,11 @@ public class testFight {
 		Battles battle = new Battles();
 //		I think these two above lines only need to be initialised once in main
 
-		myTeam.addFriend(new WaterMonster());
-		myTeam.addFriend(new FireMonster());
 		myTeam.addFriend(new RandomMonster());
-		
-		System.out.println(myTeam.sumTeamHealth());
-		
-		if (battle.fight(myTeam, new GrassMonster()) == true) {
+		myTeam.addFriend(new RandomMonster());
+		myTeam.addFriend(new RandomMonster());
+				
+		if (battle.fight(myTeam, new RandomMonster()) == true) {
 //			continue the game
 			System.out.println("The game continues");
 		} else {
