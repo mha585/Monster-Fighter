73118549bcbbd772be354b385d1f4a91f70213ca
mha585 <mha585@uclive.ac.nz>
2022-04-14 01:05:14 +1200

    Update testFight.java

--- MonsterFighter/Main/testFight.java
+++ MonsterFighter/Main/testFight.java
@@ -3,7 +3,7 @@
 //this is just a class to test fights between monsters delete later!!!!
 public class testFight {
 	public static void main(String[] args) {
-		
+
 		Team myTeam = new Team();
 		Battles battle = new Battles();
 		Player test = new Player("tester", 6, 1);
@@ -12,10 +12,10 @@ public class testFight {
 		myTeam.addFriend(new MedicalMonster());
 		myTeam.addFriend(new RandomMonster());
 		myTeam.addFriend(new RandomMonster());
-		
+
 		test.playerBag.addtoBag(new TierPlus(), 1);
 		test.playerBag.addtoBag(new BasicHeal(), 3);
-		
+
 		if (battle.fight(myTeam, new RandomMonster(), test.playerBag) == true) {
 //			continue the game
 			System.out.println("The game continues");
