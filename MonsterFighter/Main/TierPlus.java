95ecfd8b45cf98d12fa1e56092fea6394d15e1de
mha585 <mha585@uclive.ac.nz>
2022-04-14 01:28:33 +1200

    Is the nightmare over?

--- MonsterFighter/Main/TierPlus.java
+++ MonsterFighter/Main/TierPlus.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:MonsterFighter/src/TierPlus.java
 /**
  * 
  * @author Angelo  Gasmin
@@ -40,60 +39,4 @@ public class TierPlus extends Item{
 	TierPlus() {
 		super(name, price, sell ,description, whenToUse, itemEffect, statEffect);
 	}
-
-	/**
-	 * Uses Tier Plus on a chosen monster
-	 * @param myMonster			The monster whose tier will be increased
-	 */
-
-
 }
-=======
-
-/**
- * 
- * @author Angelo  Gasmin
- * @version 1.0
- */
-public class TierPlus extends Item{
-	/**
-	 * The name of TierPlus
-	 */
-	private static String name = "Tier Plus";
-	/**
-	 * The description of Tier Plus
-	 */
-	private static String description = "Use on a monster out of battle to increase your monster's Tier by 1.";
-	/**
-	 * The price of Tier Plus
-	 */
-	private static int price = 50;
-	/**
-	 * The selling price of Tier Plus
-	 */
-	private static int sell = 40;
-	
-	private static int itemEffect = 1;
-	
-	private static String statEffect = "t";
-	
-	private static String whenToUse = "out";
-	/**
-	 * The basic constructor for TierPLus
-	 * @param name				The name of Tier Plus
-	 * @param price				The price of Tier Plus
-	 * @param sell				The selling price of Tier Plus
-	 * @param description		The description of Tier Plus
-	 */
-	TierPlus() {
-		super(name, price, sell ,description, whenToUse, itemEffect, statEffect);
-	}
-
-	/**
-	 * Uses Tier Plus on a chosen monster
-	 * @param myMonster			The monster whose tier will be increased
-	 */
-
-
-}
->>>>>>> Matthew:MonsterFighter/Main/TierPlus.java
